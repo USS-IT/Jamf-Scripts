1e1506156def45fe668de56de7fb6fba6befe88a
JustATechie <louie@jhu.edu>
2023-06-28 13:54:48 -0400

    :sparkles: created function that checks what code was given from the deactivation command.

--- scripts/utilities/DeactivateAdobeLicenses.sh
+++ scripts/utilities/DeactivateAdobeLicenses.sh
@@ -3,8 +3,50 @@
 ###
 # Global Variables
 ###
-toolkitPath="/Volumes/tempAdobe/"
+toolkitPath="/Volumes/tempAdobe"
+toolkitCommand="$toolkitPath/adobe-licensing-toolkit"
+toolkitFailure="Operation Failed"
+toolkitSuccess="Operation Succeeded"
+toolkitOutput=$($toolkitCommand --deactivate | grep "$toolkitFailure")
 
-/Volumes/tempAdobe/adobe-licensing-toolkit --deactivate
+###
+# A simple logging function that prints to screen the passed type and message.
+###
+log () {
+	local code=$1
+	local message=$2
 
-//rm -rf /Volumes/tempAdobe/
+	if [ ! -z "$message" ]; then
+		echo "$code: $message"
+	fi
+}
+
+###
+# Simple function to attempt to deactivate
+###
+deactivateLicense () {
+  local output="$toolkitOutput"
+  if [ "$output" == "$toolkitFailure" ]; then
+    log "WARNING" "Deactivating licenses failed, perhaps none were present."
+
+  elif [ "$output" == "$toolkitSuccess" ]; then
+    log "INFO" "Deactivating licenses succeeded."
+
+  else
+    log "ERROR" "Unknown error was thrown when attempting to deactivate licenses, exit 10."
+    exit 10
+  fi
+}
+
+main () {
+
+  deactivateLicense
+
+	log "INFO" "Script complete, exit 0."
+	exit 0
+}
+
+# call the main function
+main "$@"
+
+#rm -rf /Volumes/tempAdobe/
